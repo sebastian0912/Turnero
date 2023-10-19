d231a0799a22c52934a5c053bb0b61152dcde1ca
Sebastian Guarnizo Campos <62730462+sebastian0912@users.noreply.github.com>
2023-10-19 06:09:25 -0500

    Seleccion y contratacion

---
+++ src/app/Roles/Seleccion/seleccion.js
@@ -0,0 +1,39 @@
+
+import { aviso } from "../../Avisos/avisos.js";
+import { urlBack } from "../../model/base.js";
+
+const uid = localStorage.getItem("idUsuario");
+
+// Capturar el h1 del titulo y perfil
+const titulo = document.querySelector('#username');
+const perfil = document.querySelector('#perfil');
+// Capturar el PERFIL y el USERNAME del local storage
+const perfilLocal = localStorage.getItem("perfil");
+const usernameLocal = localStorage.getItem("username");
+
+//Muestra en la parte superior el nombre y el perfil
+titulo.innerHTML = usernameLocal;
+perfil.innerHTML = perfilLocal;
+
+let graficas = document.getElementById("estadisticas");
+
+if (perfilLocal == "GERENCIA" ) {
+    graficas.style.display = "block";
+}
+
+
+document.getElementById('mes').addEventListener('change', function() {
+    var selectedMonth = this.value;
+    var daysDropdown = document.getElementById('dia');
+    daysDropdown.innerHTML = ''; // Borra las opciones anteriores
+
+    var daysInMonth = new Date(2023, selectedMonth, 0).getDate(); // Obtiene el número de días en el mes
+
+    for (var i = 1; i <= daysInMonth; i++) {
+        var day = i.toString().padStart(2, '0'); // Agrega un 0 si es un solo dígito
+        var option = document.createElement('option');
+        option.value = day;
+        option.textContent = day;
+        daysDropdown.appendChild(option);
+    }
+});
